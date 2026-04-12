bed197c5a89d55ec3891fa7ba336c228cd8816a7
Edson Luiz Chagas Souza <edinholuiz12@gmail.com>
2026-04-12 10:05:03 -0300

    Acionado validador de compra de itens com lista

--- aula_02.py
+++ aula_02.py
@@ -1,8 +1,10 @@
 # Execercício - O Validador de Senhas
 
+lista_item = []
 # Estrutura de repetição para cadastro de senha
 while True:
     senha = input("Digite sua senha: ")
+    # Conta quantos caracteres tem a senha digitada
     tamanho_senha = len(senha)
     # Condição para confirmar o tamanhdo da senha e seguir com o cadastro
     if tamanho_senha >= 8:
@@ -10,3 +12,12 @@ while True:
         break
     else:
         print("Senha muito curta!")
+    
+# Estrutura de repetição para a compra de 3 itens
+while len(lista_item) < 3:
+    item = input("Escolha um item: ")
+    # Adiciona os itens digitados na lista
+    lista_item.append(item)
+    print(f"Você já tem {len(lista_item)} itens.")
+
+print("Maravilha, sua compra foi concluída!")
